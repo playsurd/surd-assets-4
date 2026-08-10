59f5f5c0e3fc11ff27d937b053a279b8c83a4cd1
playsurd <312274040+playsurd@users.noreply.github.com>
2026-08-10 22:34:06 +0200

    publish kirby-soft-wet/index.js

--- kirby-soft-wet/index.js
+++ kirby-soft-wet/index.js
@@ -455,11 +455,7 @@
         outputContainerElement.hidden = true;
       }
 
-      document.addEventListener("visibilitychange", (event) => {
-        if (document.visibilityState != "visible") {
-          pause();
-        }
-      });
+      
 
       window.addEventListener("load", (event) => {
         if ((!window.oprt || !window.oprt.enterFullscreen) && (!window.chrome || !window.chrome.runtime || !window.chrome.runtime.sendMessage)) {
